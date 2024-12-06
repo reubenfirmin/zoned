9cf46a98ac270d605ba5746c428c9bdc186fd625
Reuben Firmin <rf@flavor8.com>
2024-12-06 08:57:24 -0600

    fix packaging

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    kotlin("jvm") version "2.0.20"
+    kotlin("jvm") version "2.0.21"
     id("com.github.node-gradle.node") version "7.0.1"
     `maven-publish`
     `java-gradle-plugin`
